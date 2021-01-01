e84747f1453f8f6d62e4185db54314540ca1c0d9
IVBecy <57770911+IVBecy@users.noreply.github.com>
2021-01-01 13:59:37 +0000

    Able to create new users (in code)

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -1,8 +1,37 @@
 // To generate page with express
 const express = require("express");
-const path = require("path");
 const app = express();
 const port = 8000;
+const path = require("path");
+const mongoose = require("mongoose");
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useUnifiedTopology", true);
+mongoose.set('useCreateIndex', true);
+const mongo = "localhost:27017";
+const DB = "fire_db";
+const user = require("./user");
+//Connecting database
+mongoose.connect(`mongodb://${mongo}/${DB}`).then(() => { console.log(`Connected to mongodb://${mongo}/${DB}`); }).catch((err) => console.log(err));
+//Function for finding records in the database
+const findResources = (model,record,name) => {
+  model.find({record:name},record,(err,res) => {
+    if (err) throw err;
+    console.log(res);
+  });
+  user.find({ 'username': 'John' }, 'username', function (err, res) {
+    if (err) throw err;
+    console.log(res);
+  })
+}
+// Delete all data
+const deleteAllData = async () => {
+  try {
+    await user.deleteMany();
+    console.log('All Data successfully deleted');
+  } catch (err) {
+    console.log(err);
+  }
+};
 // Serving
 app.use(express.static('public'));
 app.get("/",(req,res) => {
---
+++ public/assets/js/user.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose");
+const userSchema = new mongoose.Schema({
+  username: { type: String, unique: true},
+  password:{ type: String},
+  email: { type: String, unique: true},
+})
+module.exports = mongoose.model("user",userSchema)
