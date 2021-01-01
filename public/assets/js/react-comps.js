01423d040e7b75ffa2db6ef152bad429619c0bd4
IVBecy <57770911+IVBecy@users.noreply.github.com>
2021-01-01 10:39:13 +0000

    Added log in $ sign up buttons

--- public/assets/js/react-comps.js
+++ public/assets/js/react-comps.js
@@ -13,6 +13,7 @@ const HeaderOutsider = () => {
   return (
     <div className="container-fluid header-outside">
       <h3><i className="fas fa-fire"></i> Fire</h3>
+      <button id="sign-up-btn">Sign up</button><button id="sign-in-btn">Sign in</button>
       <div style={{clear:"both"}}></div>
     </div>
   )
